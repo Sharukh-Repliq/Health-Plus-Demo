a8d3e9b60356efa0f728c09e54f48cec3f77ea81
Sharukh Fardin <sharukh.fardin@repliq.dev>
2023-09-06 10:54:03 +0600

    More updates

--- healthplus/core/models.py
+++ healthplus/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
+from autoslug import AutoSlugField
 
-# Create your models here.
+class AbstractBaseModel(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = AutoSlugField(unique=True, populate_from='name')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return str(self.name)
